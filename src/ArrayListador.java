dad92008abbf38a11173b0ab8c15efa22607e66e
Nèstor <Nstor@LAPTOP-N2IIIU1I>
2017-05-26 19:29:08 -0300

    Primer commit

---
+++ src/ArrayListador.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ArrayListador  {
+
+	public static void main(String[] args) {
+	  int n;
+	  Scanner teclado=new Scanner(System.in);
+      System.out.println("Ingrese valor entero");
+      	n=teclado.nextInt();
+      	int m;
+      	ArrayList <Integer> array = new ArrayList <Integer> ();
+        String resp="";
+      	do{
+      		System.out.println("Ingrese valor/es");
+          	m=teclado.nextInt();
+	          	if(m>n){
+	          		array.add(m);}
+      		do{
+      			System.out.println("¿Desea seguir? si/no");
+      			resp=teclado.next();
+      		}while(!resp.equals("si") && !resp.equals("no"));
+      	}while(resp.equals("si"));
+      	
+      	System.out.println("--------------------------/n");
+      	System.out.println("Valores mayores a "+n);
+      	
+		if(array.isEmpty()){
+      	System.out.println("No los hay");}
+      	for(int r:array)
+      	{ 
+      		System.out.println(r);
+      	}
+      	
+      	teclado.close();
+	}
+
+}
